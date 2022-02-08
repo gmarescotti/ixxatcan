c848d98564286c90befee262ccded77fd2f52429
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-02-08 10:47:40 +0100

    refact: installer now runs "inside" repository

--- binary_installer.bat
+++ binary_installer.bat
@@ -19,30 +19,15 @@ IF ERRORLEVEL 1 (
 	goto end
 )
 
-cd /D %temp%
-
-set MYTEMP=RGM%RANDOM%
-
 rem try MVSC2019 then MSVC2017
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build
 
 echo %PATH%
 
-mkdir %MYTEMP%
-cd %MYTEMP%
-
-git.exe clone https://github.com/gmarescotti/ixxatcan.git 
-
 CALL vcvars64.bat
-pushd ixxatcan
 qmake
 C:\Qt\Tools\QtCreator\bin\jom.exe install
-popd
 
 :end
-@echo IXXAT build terminated, click to remove temporary files and exit
-pause
-
-rmdir /S /Q %MYTEMP%
-
+@echo IXXAT build terminated
